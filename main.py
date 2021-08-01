077928d24a3f5aeb36376a9559f2eb9abf1716c7
markotranda <marko.tranda@gmail.com>
2021-08-01 15:31:49 +0200

    complete

--- main.py
+++ main.py
@@ -1,25 +1,80 @@
 from tkinter.constants import RIGHT
+from typing import List
 import PySimpleGUI as sg
 from PySimpleGUI.PySimpleGUI import WINDOW_CLOSED
 
 class Calculator:
-    stored_value: str
     value: str
+    value_stack: List
+    for_reset: bool
 
     def __init__(self) -> None:
-        self.value = '0'
+        self.__button_reset()
 
     def __str__(self) -> str:
         return self.value
 
     def button_press(self, event):
-        if event in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.',):
-            if self.value == '0':
-                self.value = event
-            else:
-                self.value += event
+        if event in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9'):
+            self.__button_digit(event)
+        elif event == '.':
+            self.__button_comma()
         elif event == 'CE':
-            self.value = '0'
+            self.__button_reset()
+        elif event == '+/-':
+            self.__button_sign()
+        elif event in ('+', '-', '*', '/', '%'):
+            self.__button_operator(event)
+        elif event == '=':
+            self.__calculate_value_stack()
+
+    def __button_comma(self):
+        if not '.' in self.value:
+            self.value += '.'
+            
+    def __button_digit(self, input):
+        if self.value == '0' or self.for_reset:
+            self.value = input
+        else:
+            self.value += input
+        self.for_reset = False
+    
+    def __button_sign(self):
+        if float(self.value) > 0:
+            self.value = '-' + self.value
+        else:
+            self.value = self.value[1:]
+
+    def __button_operator(self, operator):
+        self.__calculate_value_stack()
+        self.value_stack.append(self.value)
+        self.value_stack.append(operator)
+        self.for_reset = True
+        
+    def __calculate_value_stack(self):
+        self.value_stack.append(self.value)
+        if len(self.value_stack) >= 3:
+            value2 = self.value_stack.pop()
+            operator = self.value_stack.pop()
+            value1 = self.value_stack.pop()
+
+            if operator == '+':
+                self.value_stack.append(f'{float(value1) + float(value2)}')
+            elif operator == '-':
+                self.value_stack.append(f'{float(value1) - float(value2)}')
+            elif operator == '*':
+                self.value_stack.append(f'{float(value1) * float(value2)}')
+            elif operator == '/':
+                self.value_stack.append(f'{float(value1) / float(value2)}')
+            elif operator == '%':
+                self.value_stack.append(f'{(float(value1)/100) * float(value2)}')
+                
+        self.value = self.value_stack.pop()
+    
+    def __button_reset(self):
+        self.value = '0'
+        self.value_stack = []
+        self.for_reset = False
 
 def main():
     calculator = Calculator()
@@ -34,7 +89,7 @@ def main():
         [sg.Column([[sg.Button('0', size=(4, 2)), sg.Button('.', size=(4, 2)), sg.Button('=', size=(4, 2))]], element_justification=RIGHT)]
     ]
 
-    window = sg.Window('Calculator', layout, size=(250, 400))
+    window = sg.Window('Calculator', layout, size=(235, 350))
 
     while True:
         event, values = window.read()
@@ -45,7 +100,6 @@ def main():
         window['-INPUT-'].update(calculator)
 
     window.close()
-
 
 if __name__ == '__main__':
     main()
